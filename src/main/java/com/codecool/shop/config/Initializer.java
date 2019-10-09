361ba8652937282810a689b3e8ea93f8826fbfba
Franek Zawadzki <franciszekezawadzki@gmail.com>
2019-10-09 13:45:03 +0200

    product and category sorting

--- src/main/java/com/codecool/shop/config/Initializer.java
+++ src/main/java/com/codecool/shop/config/Initializer.java
@@ -33,22 +33,29 @@ public class Initializer implements ServletContextListener {
 
         //setting up a new supplier
         Supplier amazon = new Supplier("Amazon", "Digital content and services");
-        supplierDataStore.add(amazon);
         Supplier lenovo = new Supplier("Lenovo", "Computers");
+        Supplier ebay = new Supplier("Ebay", "International supplier");
+        supplierDataStore.add(amazon);
         supplierDataStore.add(lenovo);
+        supplierDataStore.add(ebay);
 
         //setting up a new product category
-        ProductCategory tablet = new ProductCategory("Tablet", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
-        ProductCategory tablet2 = new ProductCategory("Tablet2", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
+        ProductCategory tablet = new ProductCategory("Tablets", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
+        ProductCategory laptop = new ProductCategory("Laptops", "Hardware", "A small, portable personal computer (PC), typically having a thin LCD or LED computer screen mounted on the inside of the upper lid");
+        ProductCategory phone = new ProductCategory("Phones", "Hardware", "Portable telephone that can make and receive calls over a radio frequency");
         productCategoryDataStore.add(tablet);
-        productCategoryDataStore.add(tablet2);
+        productCategoryDataStore.add(laptop);
+        productCategoryDataStore.add(phone);
 
         //setting up products and printing it
         productDataStore.add(new Product("Amazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon));
         productDataStore.add(new Product("Lenovo IdeaPad Miix 700", 479, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo));
         productDataStore.add(new Product("Amazon Fire HD 8", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon));
-        productDataStore.add(new Product("TEST Fire HD 8", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet2, amazon));
-        productDataStore.add(new Product("TEST Fire HD 8", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet2, amazon));
-        productDataStore.add(new Product("TEST Fire HD 8", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet2, amazon));
+        productDataStore.add(new Product("DELL", 89, "USD", "DELL's latest multipurpose laptop.", laptop, amazon));
+        productDataStore.add(new Product("Asus", 99, "USD", "Asus's latest laptop.", laptop, amazon));
+        productDataStore.add(new Product("Legion", 189, "USD", "Gaming laptop.", laptop, lenovo));
+        productDataStore.add(new Product("Iphone", 10000, "USD", "A phone.", phone, amazon));
+        productDataStore.add(new Product("Samsung", 400, "USD", "Android phone.", phone, ebay));
+        productDataStore.add(new Product("Xiaomi", 200, "USD", "Good affordable mobile phone.", phone, ebay));
     }
 }
--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -2,9 +2,12 @@ package com.codecool.shop.controller;
 
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.SupplierDao;
 import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
 import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.dao.implementation.SupplierDaoMem;
+import com.codecool.shop.model.Supplier;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -15,15 +18,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @WebServlet(urlPatterns = {"/"})
 public class ProductController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
         ProductDao productDataStore = ProductDaoMem.getInstance();
         ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+        SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
+
 
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
@@ -31,7 +41,10 @@ public class ProductController extends HttpServlet {
         context.setVariable("categories", productCategoryDataStore.getAll());
 //        context.setVariable("products", productDataStore.getBy(productCategoryDataStore.find(1)));
         context.setVariable("products", productDataStore.getAll());
+        context.setVariable("suppliers", supplierDataStore.getAll());
         engine.process("product/index.html", context, resp.getWriter());
+
+
     }
 
     // // Alternative setting of the template context
@@ -39,5 +52,54 @@ public class ProductController extends HttpServlet {
     // params.put("category", productCategoryDataStore.find(1));
     // params.put("products", productDataStore.getBy(productCategoryDataStore.find(1)));
     // context.setVariables(params);
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+        String selectedCategory = req.getParameter("selection");
+//        System.out.println(selectedCategory.matches(".*\\d.*"));
+        System.out.println(getFirstInt(selectedCategory));
+
+        ProductDao productDataStore = ProductDaoMem.getInstance();
+        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+        SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
+
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+//        context.setVariable("category", productCategoryDataStore.find(1));
+        context.setVariable("suppliers", supplierDataStore.getAll());
+
+        context.setVariable("categories", productCategoryDataStore.getAll());
+
+        if(selectedCategory.equals("0")){
+            context.setVariable("products", productDataStore.getAll());
+        }
+        else if (chechIfStringNum(selectedCategory)){
+            context.setVariable("products", productDataStore.getBy(productCategoryDataStore.find(Integer.parseInt(selectedCategory))));
+        }
+        else{
+           Supplier supplier = supplierDataStore.find(getFirstInt(selectedCategory));
+            List productDao = productDataStore.getBy(supplier);
+            System.out.println(productDao);
+            context.setVariable("products", productDao);
+    }
+
+        engine.process("product/index.html", context, resp.getWriter());
+
+
+//        response.sendRedirect("/shop");
+    }
+    private boolean chechIfStringNum(String text){
+        if(text.matches("^[0-9]*$")){
+            return true;
+        }
+        return false;
+    }
+    private int getFirstInt(String text){
+        Matcher matcher = Pattern.compile("\\d+").matcher(text);
+        matcher.find();
+        int i = Integer.parseInt(matcher.group());
+        return i;
+    }
+
 
 }
